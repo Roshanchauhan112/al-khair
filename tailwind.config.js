a6664684d1febba56c3f69080a5fc7038f521abc
YourGitHubUsername <your-email@example.com>
2025-03-05 18:20:04 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+ /** @type {import('tailwindcss').Config} */
+ export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'green-gradient': 'linear-gradient(to bottom, #276227, #98C998)',
+      },
+    },
+  },
+  plugins: [],
+}
